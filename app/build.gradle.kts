1f55ec578658d28d6b433372b44e8c4111d23327
TD lisaCI <lisa.brlt15@gmail.com>
2025-02-01 23:49:50 +0100

    Ajout LoginActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage.ktx)
     implementation(libs.play.services.location)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
